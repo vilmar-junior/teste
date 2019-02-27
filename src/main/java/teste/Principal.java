73daa6c6b90c4315674ede39bb2e7d430e5df6fb
Vilmar <vilmar.junior@edu.sc.senac.br>
2019-02-27 08:53:58 -0300

    Versão final

--- src/main/java/teste/Principal.java
+++ src/main/java/teste/Principal.java
@@ -9,8 +9,9 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JCheckBox;
-import javax.swing.JEditorPane;
 import javax.swing.JSpinner;
+import java.awt.Scrollbar;
+
 
 public class Principal {
 
@@ -47,7 +48,7 @@ public class Principal {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton btnClique = new JButton(" Clique aqui!");
+		JButton btnClique = new JButton(" N\u00E3o Clique aqui!");
 		btnClique.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
@@ -55,11 +56,14 @@ public class Principal {
 		btnClique.setForeground(Color.RED);
 		frame.getContentPane().add(btnClique, BorderLayout.CENTER);
 		
-		JCheckBox chckbxJaFez = new JCheckBox("Ja fez?");
-		frame.getContentPane().add(chckbxJaFez, BorderLayout.SOUTH);
+		JCheckBox chkFeito = new JCheckBox("Feito?");
+		frame.getContentPane().add(chkFeito, BorderLayout.SOUTH);
 		
 		JSpinner spinner = new JSpinner();
 		frame.getContentPane().add(spinner, BorderLayout.NORTH);
+		
+		Scrollbar scrollbar = new Scrollbar();
+		frame.getContentPane().add(scrollbar, BorderLayout.EAST);
 	}
 
 }
